673bf3ead106830b3274359832758f4104a91ae7
tanmayc07 <tanmayc32@gmail.com>
2020-10-04 11:28:30 +0530

    :tada: Initial Commit

---
+++ src/components/headerComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Calendar By {{ name }}</h1>
+</template>
+
+<script>
+export default {
+  setup() {
+    const name = "Tanmay";
+
+    return {
+      name,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
